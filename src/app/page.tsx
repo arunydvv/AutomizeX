a04f9f1309a53d7535e1308ae90afa4ab82d0430
arunydvv <arunyadav9704@gmail.com>
2025-01-08 17:06:46 +0530

    Infinite Testimonilas added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { ContainerScroll } from "@/components/Global/Container-Scroll-Animation";
 import { HeroParallax } from "@/components/Global/hero-parallax";
+import { InfiniteMovingCards } from "@/components/Global/InfiniteCards";
 import Navbar from "@/components/Global/Navbar";
 import { Button } from "@/components/ui/button";
+import {  products } from "@/Constants/parallax-products";
+import { testimonials } from "@/Constants/statics";
 
 
 export default function Home() {
@@ -16,13 +19,13 @@ export default function Home() {
               <div className="flex items-center flex-col">
                 <Button
                   size={'lg'}
-                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                  className="p-6 mb-8 md:mb-0 text-xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
                 >
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
                     Start For Free Today
                   </span>
                 </Button>
-                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-slate-50 to-neutral-600 font-sans font-bold pb-5">
                   Automate Your Work With AutomizeX
                 </h1>
               </div>
@@ -31,12 +34,20 @@ export default function Home() {
         </div>
       </section>
 
-      <HeroParallax
-        products={}
+
+      <InfiniteMovingCards
+        className="md:my-[28rem] mt-[-100px]"
+        items={testimonials}
+        direction="right"
+        speed="slow"
       />
+      <HeroParallax products={products} />
+      
+
 
 
     </main>
    
   );
 }
+
