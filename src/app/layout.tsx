0c7c7652dc7057da614e42656dc6bbfe6fe85983
arunydvv <arunyadav9704@gmail.com>
2025-01-08 12:04:52 +0530

    Added satoshi font/ created Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const font = DM_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "AutomizeX",
-  description: "AI-powered automation builder",
+  description: "Let AI automate your work",
 };
 
 export default function RootLayout({
@@ -26,11 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${font}  antialiased`}
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
