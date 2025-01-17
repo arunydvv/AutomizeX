5c71e5e51b4115fdde40e65a5bf631eeb97a128e
arunydvv <arunyadav9704@gmail.com>
2025-01-17 12:08:12 +0530

    Correcting Dashboard Issues

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,34 +1,68 @@
-"use client"
 
-import * as React from "react"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import React from 'react'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { menuOptions } from '@/Constants/menu'
+import clsx from 'clsx'
 
-const Sidebar = () => {
-    const [mounted, setMounted] = React.useState(false);
 
-    React.useEffect(() => {
-        setMounted(true);
-    }, []);
+type Props = {}
 
-    const pathName = usePathname();
-
-    if (!mounted) {
-        return null;
-    }
+const MenuOptions = (props: Props) => {
+    const pathName = usePathname()
 
     return (
-        <nav className='dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-2'>
-            <div className='flex flex-col gap-8 items-center justify-center ' >
+        <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+            <div className="flex items-center justify-center flex-col gap-8">
                 <Link
-                    href={'/'}
-                    className='flex font-bold flex-row'
+                    className="flex font-bold flex-row "
+                    href="/"
                 >
                     AutomizeX
                 </Link>
+                <TooltipProvider>
+                    {menuOptions.map((menuItem) => (
+                        <ul key={menuItem.name}>
+                            <Tooltip delayDuration={0}>
+                                <TooltipTrigger>
+                                    <li>
+                                        <Link
+                                            href={menuItem.href}
+                                            className={clsx(
+                                                'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
+                                                {
+                                                    'dark:bg-[#2F006B] bg-[#EEE0FF] ':
+                                                        pathName === menuItem.href,
+                                                }
+                                            )}
+                                        >
+                                            <menuItem.Component
+                                                selected={pathName === menuItem.href}
+                                            />
+                                        </Link>
+                                    </li>
+                                </TooltipTrigger>
+                                <TooltipContent
+                                    side="right"
+                                    className="bg-black/10 backdrop-blur-xl"
+                                >
+                                    <p>{menuItem.name}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </ul>
+                    ))}
+                </TooltipProvider>
+                
             </div>
+            
         </nav>
-    );
+    )
 }
 
-export default Sidebar;
+export default MenuOptions
