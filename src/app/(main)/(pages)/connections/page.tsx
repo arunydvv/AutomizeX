bac81ac83834cf020f20d44868a3e164d186489e
arunydvv <arunyadav9704@gmail.com>
2025-01-19 11:52:44 +0530

    Settings Page SetUp

--- src/app/(main)/(pages)/connections/page.tsx
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -4,7 +4,11 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>Connections</div>
+    <div className='flex flex-col gap-4 relative   '>
+      <h1 className='text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b'>
+        Connections
+      </h1>
+    </div>
   )
 }
 
--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,10 +1,25 @@
+import  ProfileForm  from '@/components/Forms/profile-form'
 import React from 'react'
-
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>Settings</div>
+    <div className='flex flex-col gap-4 relative   '>
+      
+      <h1 className='text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b'>
+        Settings
+      </h1>
+      <div className='flex flex-col gap-10 p-6'>
+        <div className='profile-pic'>
+          <h2 className='text-2xl font-bold '>User profile</h2>
+          <p className='text-base text-white/50'>Add or update your information</p>
+        </div>
+        <ProfileForm />
+
+
+
+      </div>
+    </div>
   )
 }
 
